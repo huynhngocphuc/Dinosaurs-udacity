3b41fe8aae1fbf43831ea6e696201c5f3fdede0a
Phuc <huynhngocphuc.9d@gmail.com>
2024-07-04 16:46:10 +0700

    [huynphuc] add init source

---
+++ app.js
@@ -0,0 +1,33 @@
+// Create Dino Constructor
+
+// Create Dino Objects
+
+// Create Human Object
+
+// Use IIFE to get human data from form
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
+
+// On button click, prepare and display infographic
+
+const btn = document.getElementById("btn");
+btn.onclick = () => {
+  const formElements = document.querySelectorAll(
+    "#name,#feet,#inches,#weight, #diet"
+  );
+  const [name, height_feet, height_inches, weight, diet] = formElements;
+  console.log("🚀 ~ formElements:", formElements);
+};
